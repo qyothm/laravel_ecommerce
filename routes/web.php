f76c69995f683930a4e62d0dc9735b3c814f58f8
Qusyairi Othman <qusyairi.work@gmail.com>
2022-10-20 11:48:52 +0800

    Admin Panel - Add Category

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,3 +27,5 @@ Route::middleware([
 });
 
 route::get('/redirect', [HomeController::class,'redirect']);
+Route::get('/view_category', [AdminController::class,'view_category']);
+Route::post('/add_category', [AdminController::class,'add_category']);
