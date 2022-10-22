f6077e4ada014b5b57b88255b585ab7205e7ddec
Qusyairi Othman <qusyairi.work@gmail.com>
2022-10-22 12:13:15 +0800

    Add Product

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Product;
 
 class AdminController extends Controller
 {
@@ -26,5 +27,32 @@ class AdminController extends Controller
         $data = Category::find($id);
         $data->delete();
         return redirect()->back()->with('message','Category Deleted Successfully');
+    }
+
+    public function view_product()
+    {
+        $category = Category::all();
+        return view('admin.product', compact('category'));
+    }
+
+    public function add_product(Request $request)
+    {
+        $product = new Product;
+
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->discount_price = $request->dis_price;
+        $product->category = $request->category;
+
+        $image = $request->image;
+        $imagename = time().'.'.$image->getClientOriginalExtension();
+        $request->image->move('product',$imagename);
+        $product->image = $imagename;
+
+        $product->save();
+
+        return redirect()->back()->with('message', 'Product Added Successfully');
     }
 }
