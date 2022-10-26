5f8affa5ac8625e135b197a8a2e5c985d9fc098d
Qusyairi Othman <qusyairi.work@gmail.com>
2022-10-26 21:28:00 +0800

    Show Order on Admin Page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\Order;
 
 class AdminController extends Controller
 {
@@ -96,5 +97,11 @@ class AdminController extends Controller
 
         $product->save();
         return redirect()->back()->with('message','Product Updated Successfully');;
+    }
+
+    public function order()
+    {
+        $order = Order::all();
+        return view('admin.order',compact('order'));
     }
 }
